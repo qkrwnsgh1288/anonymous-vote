b81925209b2552a0083534733243e72a6ef31aeb
junho <qkrwnsgh1288@daum.net>
2019-09-23 20:27:36 +0900

    3. types

--- x/voteservice/internal/types/types.go
+++ x/voteservice/internal/types/types.go
@@ -28,27 +28,3 @@ Voters: %v
 ProCount: %d
 NegCount: %d`, a.AgendaProposer, a.AgendaTopic, a.AgendaContent, a.Voters, a.ProCount, a.NegCount))
 }
-
-// Whois is a struct that contains all the metadata of a name
-type Whois struct {
-	Value string         `json:"value"`
-	Owner sdk.AccAddress `json:"owner"`
-	Price sdk.Coins      `json:"price"`
-}
-
-// Initial Starting Price for a name that was never previously owned
-var MinNamePrice = sdk.Coins{sdk.NewInt64Coin("nametoken", 1)}
-
-// Returns a new Whois with the minprice as the price
-func NewWhois() Whois {
-	return Whois{
-		Price: MinNamePrice,
-	}
-}
-
-// implement fmt.Stringer
-func (w Whois) String() string {
-	return strings.TrimSpace(fmt.Sprintf(`Owner: %s
-Value: %s
-Price: %s`, w.Owner, w.Value, w.Price))
-}
--- x/voteservice/internal/types/types_test.go
+++ x/voteservice/internal/types/types_test.go
@@ -5,12 +5,8 @@ import (
 	"testing"
 )
 
-func TestNewWhois(t *testing.T) {
-	a := NewWhois()
-	fmt.Println(a)
-}
 func TestNewAgenda(t *testing.T) {
 	a := NewAgenda()
-	a.Voters = append(a.Voters, "A")
+	a.Voters = append(a.Voters, "A", "B")
 	fmt.Println(a)
 }
