ba6d3f4eede1db46667097c1ac7dc0ab5292c0ea
junho <qkrwnsgh1288@daum.net>
2019-09-23 20:29:54 +0900

    6. keeper

--- x/voteservice/internal/keeper/keeper.go
+++ x/voteservice/internal/keeper/keeper.go
@@ -3,97 +3,48 @@ package keeper
 import (
 	"github.com/cosmos/cosmos-sdk/codec"
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	"github.com/cosmos/cosmos-sdk/x/bank"
 	"github.com/qkrwnsgh1288/vote-dapp/x/voteservice/internal/types"
 )
 
-// Keeper maintains the link to data storage and exposes getter/setter methods for the various parts of the state machine
-type Keeper struct {
-	CoinKeeper bank.Keeper
-	storeKey   sdk.StoreKey // Unexposed key to access store from sdk.Context
-	cdc        *codec.Codec // The wire codec for binary encoding/decoding.
+type VoteKeeper struct {
+	storekey sdk.StoreKey
+	cdc      *codec.Codec
 }
 
-// NewKeeper creates new instances of the voteservice Keeper
-func NewKeeper(coinKeeper bank.Keeper, storeKey sdk.StoreKey, cdc *codec.Codec) Keeper {
-	return Keeper{
-		CoinKeeper: coinKeeper,
-		storeKey:   storeKey,
-		cdc:        cdc,
+func NewVoteKeeper(storeKey sdk.StoreKey, cdc *codec.Codec) VoteKeeper {
+	return VoteKeeper{
+		storekey: storeKey,
+		cdc:      cdc,
 	}
 }
 
-func (k Keeper) SetWhois(ctx sdk.Context, name string, whois types.Whois) {
-	if whois.Owner.Empty() {
+func (v VoteKeeper) SetAgenda(ctx sdk.Context, agendaTopic string, agenda types.Agenda) {
+	if agenda.AgendaProposer.Empty() {
 		return
 	}
-	store := ctx.KVStore(k.storeKey)
-	store.Set([]byte(name), k.cdc.MustMarshalBinaryBare(whois))
+	store := ctx.KVStore(v.storekey)
+	store.Set([]byte(agendaTopic), v.cdc.MustMarshalBinaryBare(agenda))
 }
-func (k Keeper) GetWhois(ctx sdk.Context, name string) types.Whois {
-	store := ctx.KVStore(k.storeKey)
-	if !k.IsNamePresent(ctx, name) {
-		return types.NewWhois()
+func (v VoteKeeper) GetAgenda(ctx sdk.Context, agendaTopic string) types.Agenda {
+	store := ctx.KVStore(v.storekey)
+	if v.IsTopicPresent(ctx, agendaTopic) {
+		return types.NewAgenda()
 	}
-	bz := store.Get([]byte(name))
-	var whois types.Whois
-	k.cdc.MustUnmarshalBinaryBare(bz, &whois)
-	return whois
-}
-func (k Keeper) DeleteWhois(ctx sdk.Context, name string) {
-	store := ctx.KVStore(k.storeKey)
-	store.Delete([]byte(name))
+	bz := store.Get([]byte(agendaTopic))
+	var agenda types.Agenda
+	v.cdc.MustUnmarshalBinaryBare(bz, &agenda)
+	return agenda
 }
 
-// ResolveName - returns the string that the name resolves to
-func (k Keeper) ResolveName(ctx sdk.Context, name string) string {
-	return k.GetWhois(ctx, name).Value
+func (v VoteKeeper) GetAgendaTopic(ctx sdk.Context, agendaTopic string) string {
+	return v.GetAgenda(ctx, agendaTopic).AgendaTopic
 }
 
-// SetName - sets the value string that a name resolves to
-func (k Keeper) SetName(ctx sdk.Context, name string, value string) {
-	whois := k.GetWhois(ctx, name)
-	whois.Value = value
-	k.SetWhois(ctx, name, whois)
+func (v VoteKeeper) IsTopicPresent(ctx sdk.Context, agendaTopic string) bool {
+	store := ctx.KVStore(v.storekey)
+	return store.Has([]byte(agendaTopic))
 }
-
-// HasOwner - returns whether or not the name already has an owner
-func (k Keeper) HasOwner(ctx sdk.Context, name string) bool {
-	return !k.GetWhois(ctx, name).Owner.Empty()
-}
-
-// GetOwner - get the current owner of a name
-func (k Keeper) GetOwner(ctx sdk.Context, name string) sdk.AccAddress {
-	return k.GetWhois(ctx, name).Owner
-}
-
-// SetOwner - sets the current owner of a name
-func (k Keeper) SetOwner(ctx sdk.Context, name string, owner sdk.AccAddress) {
-	whois := k.GetWhois(ctx, name)
-	whois.Owner = owner
-	k.SetWhois(ctx, name, whois)
-}
-
-// GetPrice - gets the current price of a name
-func (k Keeper) GetPrice(ctx sdk.Context, name string) sdk.Coins {
-	return k.GetWhois(ctx, name).Price
-}
-
-// SetPrice - sets the current price of a name
-func (k Keeper) SetPrice(ctx sdk.Context, name string, price sdk.Coins) {
-	whois := k.GetWhois(ctx, name)
-	whois.Price = price
-	k.SetWhois(ctx, name, whois)
-}
-
-// Check if the name is present in the store or not
-func (k Keeper) IsNamePresent(ctx sdk.Context, name string) bool {
-	store := ctx.KVStore(k.storeKey)
-	return store.Has([]byte(name))
-}
-
-// Get an iterator over all names in which the keys are the names and the values are the whois
-func (k Keeper) GetNamesIterator(ctx sdk.Context) sdk.Iterator {
-	store := ctx.KVStore(k.storeKey)
+func (v VoteKeeper) GetTopicsIterator(ctx sdk.Context) sdk.Iterator {
+	store := ctx.KVStore(v.storekey)
 	return sdk.KVStorePrefixIterator(store, []byte{})
 }
